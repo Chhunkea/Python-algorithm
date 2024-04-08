6d4f19cd51445c456d77151efe567d104da3ebbe
Chhunkea <kimchhunkea64@gmail.com>
2024-04-08 11:24:20 -0700

    Add new feat of python algorithm

---
+++ Python Ordering sort()/main.py
@@ -0,0 +1,12 @@
+def solve(s):
+    s.sort()
+
+    while len(s) > 0:
+        mid = (len(s) - 1) // 2
+        print(s, mid, s[mid])# print(s[mid], end = ' ') 
+        s.remove(s[mid])
+    print()
+
+N = int(input())
+S = list(map(int, input().split()))
+solve(S)
